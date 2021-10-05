81c7286a51f61a38e716aa9ceb7c2a33ffc14b83
Javier Quiroga Almeida <quiroga218@gmail.com>
2021-10-05 14:53:55 -0500

    Modifiacaione

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 var myCarousel = document.querySelector('#myCarousel');
 var carousel = new bootstrap.Carousel(myCarousel);
+
+/* Funciones para cerrar el search windows */
+
+
+let removingSearch = function(){
+    document.querySelector("#search_window").classList.remove("on");
+}
+let addingSearch = function(){
+    document.querySelector("#search_window").classList.add("on");
+}
+window.addEventListener('keydown', function(e){
+     if(e.key == 'Escape'){
+        removingSearch();
+     }
+})
+document.querySelector("#btn-search").addEventListener('click', function(e){
+    addingSearch();
+});
+
+document.querySelector("#close-search").addEventListener('click', function(e){
+    removingSearch();
+});
+
+
+
+   
